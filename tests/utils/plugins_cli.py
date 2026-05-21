40e513a1dac4e0b93d869997fdcde45ca894f1f2
Puneet Dixit <236133619+puneetdixit200@users.noreply.github.com>
2026-05-21 15:17:51 +0530

    Keep dummy plugins on legacy setup builds

--- tests/utils/plugins_cli.py
+++ tests/utils/plugins_cli.py
@@ -76,13 +76,6 @@ class Plugin:
             )
             '''))
 
-        with open(self.path / 'pyproject.toml', 'w') as stream:
-            stream.write(textwrap.dedent('''
-            [build-system]
-            requires = ["setuptools<80", "wheel"]
-            build-backend = "setuptools.build_meta"
-            '''))
-
         with open(self.path / (self.import_name + '.py'), 'w') as stream:
             stream.write('from httpie.plugins import *\n')
             stream.writelines(
